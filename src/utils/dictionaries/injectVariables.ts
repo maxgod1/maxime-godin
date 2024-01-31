d7918596295bee454998999e24a78fc58445b259
Maxime Godin <maxgod1@gmail.com>
2024-01-31 12:39:27 +0900

    posthog

--- src/utils/dictionaries/injectVariables.ts
+++ src/utils/dictionaries/injectVariables.ts
@@ -1,7 +1,7 @@
 export default function injectVariables(string: string, variables: any) {
   let newString = string;
-  (Object.keys(variables || {}) || []).forEach((key) => {
-    newString = newString.replace(new RegExp(`{{*${key}*}}`, "gi"), variables[key]);
+  (Object.keys(variables || {}))?.forEach((key) => {
+    newString = newString?.replace(new RegExp(`{{*${key}*}}`, "gi"), variables[key]);
   });
   return newString;
 }
