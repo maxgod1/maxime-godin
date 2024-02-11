3473b2f9a57d01b9aefd8e0d550f15ddb57483cd
Maxime Godin <maxgod1@gmail.com>
2024-02-11 12:03:51 +0900

    contact icon

--- src/app/[lang]/contact/page.tsx
+++ src/app/[lang]/contact/page.tsx
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import { GlobalContext } from "../providers";
 import Button from "../_components/Button";
 import Link from "next/link";
-import { EnvelopeIcon } from "@heroicons/react/24/solid";
+import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
 
 export default function Stack() {
   const { strings } = useContext(GlobalContext);
@@ -12,7 +12,7 @@ export default function Stack() {
     <div className="flex flex-col items-center mx-auto max-w-[800px] w-full">
       <p className="lg:text-3xl lg:px-0 text-xl font-bold lg:mb-8 mb-6 mt-10 px-6 text-center fade-in duration-500">{strings.contact_me}</p>
       <Link href="mailto:maxmgodin@gmail.com" target="_blank" rel="noopener noreferrer">
-        <Button title={strings.contact} Icon={EnvelopeIcon} />
+        <Button title={strings.contact} Icon={PaperAirplaneIcon} />
       </Link>
     </div>
   );
